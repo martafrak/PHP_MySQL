49add11d6ee01abffd1c54d53c6ae8ec0b67c2f7
martafrak <marta.frak94@gmail.com>
2018-11-03 11:40:54 +0100

    updated login and registration files

--- PHP/3. login/login.php
+++ PHP/3. login/login.php
@@ -27,33 +27,41 @@ require_once "connect.php"; #added file where I included all data to connection
             
             #protection ->SQL injection (IMPORTANT -> use sprintf in query!!!) 
             $login = htmlentities($login, ENT_QUOTES, "UTF-8");
-            $password = htmlentities($password, ENT_QUOTES, "UTF-8");
         
             #query checks login and password in database
             #%s - it's string
-            if ($result = @$connection->query(sprintf("SELECT * FROM users WHERE login='%s' AND password='%s'", mysqli_real_escape_string($connection,$login), mysqli_real_escape_string($connection,$password))))
+            if ($result = @$connection->query(sprintf("SELECT * FROM users WHERE login='%s'", mysqli_real_escape_string($connection,$login))))
                 {
                     $num_users = $result->num_rows; #if it is 1 -> password and login are correct
                         if($num_users>0)
                             {
-                                $_SESSION['active'] = true; #if users is active, it set true in index.php
-                                $row = $result->fetch_assoc(); #save data from result /SESSION because i want to use these data in page.php
-                                $_SESSION['ID'] = $row['ID'];
-                                $_SESSION['user'] = $row['login'];
-                                $_SESSION['email'] = $row['email'];
-                                $_SESSION['level'] = $row['level'];
-                                $_SESSION['city'] = $row['city'];
-                                $_SESSION['discount'] = $row['discount'];
-                                $_SESSION['premium'] = $row['premium'];
-                            
-                                unset($_SESSION['error']);
-                                $result->close(); #or free()/free_result()
-                                header('Location: page.php');
-                                
+                                $row = $result->fetch_assoc(); #catch data from result 
+                                #check password_hash
+                                if(password_verify($password,$row['password']))
+                                    {
+                                        $_SESSION['active'] = true; #if users is active, it set true in index.php
+                                        #save data from result/SESSION because i want to use these data in page.php
+                                        $_SESSION['ID'] = $row['ID'];
+                                        $_SESSION['user'] = $row['login'];
+                                        $_SESSION['email'] = $row['email'];
+                                        $_SESSION['level'] = $row['level'];
+                                        $_SESSION['city'] = $row['city'];
+                                        $_SESSION['discount'] = $row['discount'];
+                                        $_SESSION['premium'] = $row['premium'];
+
+                                        unset($_SESSION['error']);
+                                        $result->close(); #or free()/free_result()
+                                        header('Location: page.php');
+                                    }
+                                else #wrong password 
+                                    {
+                                        $_SESSION['error'] ='Incorrect password';
+                                        header('Location: index.php');
+                                    }
                             }
-                        else #wrong login or password (because I don't have result)
+                        else #wrong login (because I don't have result)
                             {
-                                $_SESSION['error'] ='Incorrect login or password';
+                                $_SESSION['error'] ='Incorrect login';
                                 header('Location: index.php');
                             }
                 }
